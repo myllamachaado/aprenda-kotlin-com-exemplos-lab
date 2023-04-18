e3071dbb62d0334247ae3e90b4844d8561593c2a
ludmila.machado <myllamachaado@gmail.com>
2023-04-18 18:08:39 -0300

    Desafio Kotlin Digital Innovation One

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Usuario(var nome : String)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var alunos : List<Usuario>) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        for(u in usuarios){ inscritos.add(u) }
+        alunos = inscritos
     }
+    override fun toString(): String = "Nome: " + nome +
+                                "\nConteúdos: " + conteudos +
+                                "\nAlunos: " + alunos
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    // Cria conteúdos
+    val PY001 = ConteudoEducacional(nome = "Pyton I", nivel = Nivel.BASICO)
+    val PY002 = ConteudoEducacional(nome = "Pyton II", nivel = Nivel.DIFICIL)
+    val ES001 = ConteudoEducacional(nome = "Estatística I", nivel = Nivel.BASICO)
+    val ES002 = ConteudoEducacional(nome = "Estatística II", nivel = Nivel.INTERMEDIARIO)
+    val BI001 = ConteudoEducacional(nome = "Power BI I", nivel = Nivel.BASICO)
+    val JV001 = ConteudoEducacional(nome = "Java I", nivel = Nivel.BASICO)
+    val JV002 = ConteudoEducacional(nome = "Java II", nivel = Nivel.DIFICIL)
+    val DB001 = ConteudoEducacional(nome = "Banco de dados I", nivel = Nivel.BASICO)
+    val SB001 = ConteudoEducacional(nome = "Spring MVC I", nivel = Nivel.BASICO)
+
+    // Cria formações
+    val formacaoPython = Formacao(nome = "Cientista de dados utilizando Python",
+        conteudos = listOf(PY001, PY002, ES001, ES002, BI001),
+        alunos = listOf())
+    val formacaoJava = Formacao(nome = "Desenvolvedor Java Back-End",
+        conteudos = listOf(JV001, JV002, DB001, SB001),
+        alunos = listOf())
+
+    // Cria usuarios
+    val aluno1 = Usuario(nome = "Aline")
+    val aluno2 = Usuario(nome = "Júlia")
+    val aluno3 = Usuario(nome = "Matheus")
+    val aluno4 = Usuario(nome = "Joana")
+
+    // Matricula alunos
+    formacaoPython.matricular(aluno1, aluno3)
+    formacaoJava.matricular(aluno2, aluno4)
+
+    print(formacaoPython)
+    print("\n" + formacaoJava)
+
 }
